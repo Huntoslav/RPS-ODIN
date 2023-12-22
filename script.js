b0b3d08f0859a2aa4967dde4b6bef231d6486608
Huntoslav <tomas.michalekk@centrum.cz>
2023-12-22 22:07:56 +0100

    choices added function playRound added (just return conosle.log now)

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice())
+
+const choices = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
+}
+
+function playRound(playerSelection, computerSelection){
+    playerSelection = prompt("Enter rock or paper or scissors")
+    computerSelection = getComputerChoice().toLocaleLowerCase()
+    if(playerSelection === computerSelection){
+        console.log(`Its a TIE you both chose ${playerSelection}`)
+    } else if (choices[playerSelection] === computerSelection){
+        console.log(`You won! Your ${playerSelection} beats computers ${computerSelection}`)
+    } else {
+        console.log(`You lose! Computers ${computerSelection} beats your ${playerSelection}`)
+    }
+}
+
+playRound()
