6c3d73cf1d62e50e8f49dac70a4479abb2899b44
Huntoslav <tomas.michalekk@centrum.cz>
2024-01-09 21:54:39 +0100

    ui created // all button are working // showing score on the page // result of 5 round added // new Game button added

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ const choices = {
     scissors: "paper"
 }
 
-
+let playerScore = 0
+let computerScore = 0
 
 function playRound(playerSelection){
     
@@ -24,10 +25,13 @@ function playRound(playerSelection){
     if(playerSelection === computerSelection){
          return `Its a TIE you both chose ${playerSelection}`
     } else if (choices[playerSelection] === computerSelection){
+        playerScore++
         return `You won! Your ${playerSelection} beats computers ${computerSelection}`
     } else if(choices[computerSelection] === playerSelection){
+        computerScore++
         return `You lose! Computers ${computerSelection} beats your ${playerSelection}`
     }
+   
 }
 
 let rock = document.querySelector(".rock").addEventListener("click", function(e){
@@ -45,12 +49,78 @@ let paper = document.querySelector(".paper").addEventListener("click", function(
 })
 
 let scissors = document.querySelector(".scissors").addEventListener("click", function(e){
+    playerSelection = "scissors"
     let result =playRound(playerSelection)
     console.log(result)
     game(result)
+
 })
 
 function game(gameResult){
+
+    
     let showResult = document.querySelector(".myDiv")
     showResult.textContent = gameResult
+
+    let playerElement = document.querySelector(".playerScore")
+    let computerElement = document.querySelector(".computerScore")
+
+    playerElement.textContent = `Player Score: ${playerScore}`
+    computerElement.textContent = `Computer Score: ${computerScore}`
+
+    endGame()
+
+
 }
+
+
+function endGame() {
+    if (playerScore === 5) {
+        let result = document.createElement("p");
+        result.textContent = `CG! YOU WON!`
+        let winResultElement = document.querySelector(".gameResultWin")
+        if (winResultElement) {
+            winResultElement.appendChild(result)
+        }
+        newGame()
+    } else if (computerScore === 5) {
+        let result = document.createElement("p")
+        result.textContent = `Sadly, YOU LOST! :(`
+        let loseResultElement = document.querySelector(".gameResultLose")
+        if (loseResultElement) {
+            loseResultElement.appendChild(result)
+        }
+        newGame()
+    }
+}
+
+
+function newGame(){
+    let newGameButton = document.createElement("button")
+    newGameButton.textContent = "NEW GAME"
+    newGameButton.id = "novaHra"
+    document.querySelector(".newGame").appendChild(newGameButton)
+
+    let novaHraElement = document.getElementById("novaHra")
+    novaHraElement.addEventListener("click", function(e){
+        playerScore = 0
+        computerScore = 0
+        game()
+
+        let parentElement = document.querySelector(".newGame")
+        parentElement.removeChild(newGameButton)
+
+        let winResultElement = document.querySelector(".gameResultWin")
+        let loseResultElement = document.querySelector(".gameResultLose")
+
+        if(winResultElement){
+            parentElement.removeChild(winResultElement)
+        }
+
+        if(loseResultElement){
+            parentElement.removeChild(loseResultElement)
+        }
+
+    })
+}
+
